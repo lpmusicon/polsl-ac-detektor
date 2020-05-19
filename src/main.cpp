13663bbef5f8a28426cc1835bbf0cf03e99fa4f1
Grzegorz Mrózek <lpmusicon@gmail.com>
2020-05-19 21:03:01 +0200

    TODO writeup

--- src/main.cpp
+++ src/main.cpp
@@ -2,117 +2,41 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+/*
+ * Długość SSID: max 32 znaki! minimum 1
+ * Password: 63 znaki dla WPA2 <3, minimum 8
+*/
 const char ssid[] = "UPC0444679";
 const char password[] = "x58nzmvfTzrf";
-
-const char* broker = "192.168.0.150";
-const char* topicLed = "esp/test";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
 
 const uint8_t PIN_LED = 2;
 const uint8_t PIN2_LED = 16;
 
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("esp/test", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+/* 
+ * Setup
+ * 
+ * Trzeba:
+ *  > Sprawdzić czy jest konfig
+ *  > > Jak jest to wczytać i witaj świecie
+ *  > > W innym wypadku trzeba załadować konfigurację podstawową i dać użytkownikowi się wykazać
+ *  > Odpalić WiFi dla użytkownika
+ *  > Połączyć się z modułem GSM
+ *  > Muszę sprawdzać poziom zasilania (dzielnik napięcia na rezystorze wystarczy 1/3 - 2/3)
+ *  > Pilnować pinu zasilania (też dzielnik napięcia 5V - 1/3 2/3)
+*/
 
 void setup() {
-  Serial.begin(74880);
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN2_LED, OUTPUT);
-
-  setup_wifi();
-  client.setServer(broker, 1883);
-  client.setCallback(callback);
+    // checkConfig()
+    // if(exists)
+    // configure
+    // else setup
+    // wifiSetup
+    // gsmSetup
+    // pinCheck (battery + ac)
+    // serverSetup
 }
 
-bool enabled = false;
 
 void loop() {
-  Serial.println("Hello world");
-  digitalWrite(PIN_LED, enabled);
-  digitalWrite(PIN2_LED, !enabled);
-  enabled = !enabled;
-  delay(500);
 
-  if (!client.connected()) { reconnect(); }
-  client.loop();
-
-  unsigned long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
-  }
 }
